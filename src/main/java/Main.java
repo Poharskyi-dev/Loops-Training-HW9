9856e42ea52a0a32cda3b656317def65a5f24cdd
Poharskyi <avpog.box@gmail.com>
2024-10-14 22:09:43 +0200

    Final code add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,4 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Please write array length:");
+        Scanner Input = new Scanner(System.in);
+        int arrayLength = Input.nextInt();
+
+        int[] newArr = new int[arrayLength];
+
+        for (int i = 0; i < arrayLength; i++) {
+            System.out.println("Please write index " + i + " array length:");
+            newArr[i] = Input.nextInt();
+        }
+        System.out.println("Numbers that are divisible by 2:");
+        for (int i = 0; i < newArr.length; i++) {
+            if (newArr[i] % 2 == 0) {
+                System.out.println(newArr[i]);
+            }
+        }
+    }
 }
